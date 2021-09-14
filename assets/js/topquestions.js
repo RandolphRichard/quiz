7c077f33eda261f37aeb3d7223ae1efd93b94277
Randolph Richard <randolphrichard9@gmail.com>
2021-09-14 09:17:28 -0400

    added all the question

--- assets/js/topquestions.js
+++ assets/js/topquestions.js
@@ -1,58 +1,64 @@
-// list of all questions, choices, and answers
+// We will find all questions, choices, and answers for the quiz
 var questions = [
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What is meant by full stack development?",
+    choices: ["A web developer or engineer who works with both the front and back ends of a website or application",
+     "Someone who works with the Back End — or server side — of the application as well as the Front End, or client side.",
+      "Must have some skills in a wide variety of coding niches, from databases to graphic design and UI/UX management",
+       "All the above"],
+    answer: "All the above"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What does ' || ' stands for in Javascript language?  ",
+    choices: ["And", "Or", "For", "If" ,"All the above"],
+    answer: "Or"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What does ' && ' stands for in Javascript language?",
+    choices: ["And", "Or", "For", "If" ,"All the above"],
+    answer: "And"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What does isNaN stands for?",
+    choices: ["Not-a-Name", "Not-a-Number", "No-action-Necessary", "No-action-Needed"],
+    answer: "Not-a-Number"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "How to link a style sheet(CSS) file to your HTML File?",
+    choices: ["<link rel='stylesheet' type='text/css' href='example.css'/>",
+     "<link rel=stylesheet type=text/css href=example.css/>",
+      "<link rel-'stylesheet' type-'text/css' href-'example.css'/>",
+       "Non of the above"],
+    answer: "<link rel='stylesheet' type='text/css' href='example.css'/>"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "Can you link more than one style sheet on HTML file?",
+    choices: ["Yes", "No"],
+    answer: "Yes"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What does CSS stands for?",
+    choices: ["Cascading Style Sheet", "Custom Style Sheet", "Colorful Style Symbols", "Core System Software"],
+    answer: "Cascading Style Sheet"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What does HTML stands for?",
+    choices: ["HyperText Markup Language", "Hypertext Meta-Language", "Hyper Type Machine Language", "None of the above"],
+    answer: "HyperText Markup Language"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "Can you link more than one Javascript file on an HTML file?",
+    choices: ["Yes", "No"],
+    answer: "Yes"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What method do we use on CSS file to comment?",
+    choices: ["/*  */", "//", "<!---->"],
+    answer: "/*  */"
   },
   {
-    title: "",
-    choices: ["", "", "", ""],
-    answer: ""
+    title: "What method do we use on HTML file to comment?",
+    choices: ["/*  */", "//", "<!---->"],
+    answer: "<!---->"
   }
 ];
