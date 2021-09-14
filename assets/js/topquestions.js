c8ad50afe4bb44669c00633e7fb6963d4f3356ac
Randolph Richard <randolphrichard9@gmail.com>
2021-09-13 22:24:31 -0400

    first commit

---
+++ assets/js/topquestions.js
@@ -0,0 +1,58 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  },
+  {
+    title: "",
+    choices: ["", "", "", ""],
+    answer: ""
+  }
+];
